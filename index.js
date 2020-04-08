33d38a0abd0cc36540874b25b69da55554f22e6d
TheUberKid <zekixu@gmail.com>
2020-04-07 22:20:43 -0700

    reviving mr rainbow

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag} on hour ${new Date().getHours()}!`);
   client.user.setActivity('Club Penguin');
 
+  /**
   // daily cp meme
   if(new Date().getHours() === 4 && !sentMemeToday){
     sendDailyMeme('473349078013444096');
@@ -22,10 +23,11 @@ client.on('ready', () => {
       sentMemeToday = false;
     }
   }, 1000 * 60 * 60); // 1000 * 60 * 60
+  **/
 });
 
 client.on('message', msg => {
-  if(msg.author.username === 'WhatIsACore' && msg.author.discriminator === '6858' && msg.content === 'channel')
+  if(msg.author.username === 'zeki' && msg.author.discriminator === '6858' && msg.content === 'channel')
     console.log(msg.channel.id);
 
   if(msg.content.slice(0, prefix.length) === prefix){
@@ -34,14 +36,13 @@ client.on('message', msg => {
 
     switch (cmd){
       case 'help':
-        msg.reply('go help yourself');
+        msg.reply('hahahahaha fuck you');
         break;
       case 'ping':
         msg.reply('pong');
         break;
-      case 'meme':
-        var url = 'https://whatisacore.github.io/cpmemes/' + memesList[Math.floor(Math.random() * memesList.length)];
-        msg.reply({files: [url]});
+      case 'isclubpenguinback':
+        msg.reply('no ;(');
         break;
       default:
         msg.reply('idk what that means, try ' + prefix + ' help');
@@ -60,89 +61,3 @@ app.get('/', function(req, res){
 });
 
 serv.listen(process.env.PORT || 5000);
-
-
-var sentMemeToday = false;
-function sendDailyMeme(server){
-  var url = 'https://whatisacore.github.io/cpmemes/' + memesList[Math.floor(Math.random() * memesList.length)];
-  client.channels.get(server).send('today\'s club penguin meme:', {files: [url]});
-  sentMemeToday = true;
-}
-
-var memesList = [
-  'ayy lmao.png',
-  'bad penguiz.jpg',
-  'berry rude.jpg',
-  'birmingham.jpg',
-  'bodies.jpg',
-  'breaking bad.jpg',
-  'calculations.jpg',
-  'cannibalism.jpg',
-  'casting.jpg',
-  'coffee.jpg',
-  'coin.png',
-  'costume.jpg',
-  'crossover.png',
-  'crystal ball.jpeg',
-  'cult.png',
-  'cum here.webp',
-  'dicks.png',
-  'dildo.png',
-  'dont let go.jpg',
-  'fbi.jpg',
-  'freedom.jpg',
-  'gang.jpg',
-  'global warming.jpg',
-  'growing up.jpg',
-  'hug.png',
-  'i could kill u right now.jpg',
-  'jews.jpg',
-  'kkk.png',
-  'lava.jpg',
-  'lettuce.jpg',
-  'locked up.jpg',
-  'magnets.png',
-  'money.jpg',
-  'new chef.jpg',
-  'niggerhentai.png',
-  'nine one one.jpg',
-  'no one cares.jpg',
-  'no she meant me.jpg',
-  'ok.png',
-  'oppression.png',
-  'oxygen.jpg',
-  'peng-win.jpg',
-  'pirate hitler.png',
-  'puffle.png',
-  'return of christ.jpg',
-  'rip.jpg',
-  'romeo.jpg',
-  'rub it.jpg',
-  'safe.jpg',
-  'secret agent.jpg',
-  'shit.jpg',
-  'snow.jpg',
-  'stars.jpg',
-  'stop communism.png',
-  'stop screaming.jpeg',
-  'stranded.jpg',
-  'strangerdanger.jpg',
-  'stud.jpg',
-  'suicide.jpg',
-  'swastika.png',
-  'taco bell supreme combo.png',
-  'tampons.jpg',
-  'thank you all for coming.jpg',
-  'that\'s not on the menu.jpg',
-  'third flipper.jpg',
-  'tina.jpg',
-  'tyrone.jpg',
-  'unstable.jpg',
-  'waddlin funny.jpg',
-  'weeaboo.jpg',
-  'weed.jpg',
-  'what men really want.jpg',
-  'what the fuck purple.jpg',
-  'WHAT??????????!!!!!!!!!!.jpg',
-  'which is cooler.png'
-];
