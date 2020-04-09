5e39457dc5a1ac603cd2c1347664904b9cf930b7
TheUberKid <zekixu@gmail.com>
2020-04-08 17:09:35 -0700

    changed admin prefix

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ client.on('message', msg => {
     cache = [];
   }
 
-  if(msg.author.username === 'zeki' && msg.author.discriminator === '6858' && msg.content.slice(0, 4) === 'sudo'){
+  if(msg.author.username === 'zeki' && msg.author.discriminator === '6858' && msg.content.slice(0, 4) === 'œ'){
     var content = msg.content.substring(5);
     var args = content.split(' ');
     var cmd = args[0];
