f00953568ddb4a6a21876c07bc0eec952a9b24b6
fujirinngo <60482926+fujirinngo@users.noreply.github.com>
2020-08-04 00:40:08 -0700

    Update index.js

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ serv.listen(config.PORT || 5000);
 
 // korean matching regex
 const korean =/[\u1100-\u11FF\u3130-\u318F\uA960-\uA97F\uAC00-\uD7AF\uD7B0-\uD7FF]/ugi;
-const japanese_char =/[\u3000-303f\u3040-u309f]/ugi;
+const japanese_char =/[\u3000-\u303f\u3040-\u309f]/ugi;
 var cache = [];
 function circ(key, value){
   if(typeof value === 'object' && value !== null) {
